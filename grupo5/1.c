0852768b2a98cffb5366c073a6faa98a2df2fe71
New-Angelo <54410977+New-Angelo@users.noreply.github.com>
2019-08-22 17:19:58 -0300

    Create 1.c

---
+++ grupo5/1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+
+    printf("**RESPOSTAS**\n");
+    printf("R: a) a=5 b=5\n");
+    printf("R: b) a=5 b=7 *c=7\n");
+    printf("R: c) *c=3 *d=2\n");
+    printf("R: d) a=37 b=10 c=38 d=B\n");
+    
+    return 0;
+}
